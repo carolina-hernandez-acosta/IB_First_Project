41b47abe5d1abd023ff27f5330fed31e7d2e79b7
100162848 <carolina.hernandezandrea@gmail.com>
2023-09-01 08:50:07 -0500

    hello new

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,26 @@ public class Main {
 
 //println is one of three ways to output; print can also be used
         //what is the difference?
+        System.out.println("\t*");
+        System.out.println("   ooo");
+        System.out.println("  ooooo");
 
-        System.out.println("(
-        System.out.println("(' x ')");
-        //System.out.println("")
+
+        System.out.println(" (A___A)");
+        System.out.println(" (' x ')");
+        System.out.println(" ( ( ) )");
+        System.out.println("o(=(.)=)o");
+        System.out.println(" (O)_(O)");
+        System.out.println(" ");
+
+        System.out.print(" |_ ");
+        System.out.println(" _|");
+        System.out.print(" |_ ");
+        System.out.println(" _|");
+        System.out.print(" |_ ");
+        System.out.println(" _|");
+        System.out.print("WVW");
+        System.out.println("  WVW");
     }
 }
+
